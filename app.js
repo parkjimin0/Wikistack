ecab5fbf22e0dbac1628926982e781817aaf45dd
Keyairra Wright <keyairra.wright@gmail.com>
2018-08-03 15:44:57 -0400

    set up sequelize

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 const main = require('./views/main');
+const models = require('./models');
 
 const app = express();
 
@@ -12,6 +13,16 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
   res.send(main());
 });
+
+const table = async() => {
+  await models.Page.sync();
+  await models.User.sync();
+}
+
+models.db.sync({force: true});
+table();
+
+
 
 const PORT = 3000;
 
